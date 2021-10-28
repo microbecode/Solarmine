308422a9f5181769fae9f16a3ad3acceae39ad70
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2021-10-28 17:26:10 +0300

    reward unit tests

--- contracts/Rewards.sol
+++ contracts/Rewards.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.0;
 
 import "./IMyToken.sol";
+import "hardhat/console.sol";
 
 contract Rewards {
     IMyToken private _underlying;
@@ -10,26 +11,19 @@ contract Rewards {
     }
 
     function notifyRewards() public payable {
-        address[] memory holders = _underlying.getHolders();
-        uint256 supply = _underlying.totalSupply();
         uint256 toShare = address(this).balance;
-        for (uint256 i = 0; i < holders.length; i++) {
-            uint256 bal = _underlying.balanceOf(holders[i]);
-            uint256 share = toShare * (bal / supply);
-            // TODO: what if receiver reverts
-            (bool success, ) = holders[i].call{value: share, gas: 3000}("");
+        if (toShare > 0) {
+            address[] memory holders = _underlying.getHolders();
+            uint256 supply = _underlying.totalSupply();
+            for (uint256 i = 0; i < holders.length; i++) {
+                uint256 bal = _underlying.balanceOf(holders[i]);
+                uint256 share = (toShare * bal) / supply;
+                console.log("sharing %s", share);
+                // TODO: what if receiver reverts
+                (bool success, ) = holders[i].call{value: share, gas: 3000}("");
+            }
         }
     }
 
-    /*     function claim() public {
-        uint256 myBalance = _underlying.balanceOf(msg.sender);
-        uint256 total = _underlying.totalSupply();
-        uint256 percentage = myBalance / total;
-
-        uint256 currBalance = _rewardToken.balanceOf(address(this));
-        if (currBalance > _prevBalance) {
-            
-        }
-        _prevBalance = currBalance;
-    } */
+    // TODO: run in batches
 }
--- test/Rewards.test.ts
+++ test/Rewards.test.ts
@@ -6,7 +6,7 @@ import { expect } from "chai";
 require("@nomiclabs/hardhat-waffle");
 
 describe("Token", function () {
-  let accounts: SignerWithAddress[];
+  /*let accounts: SignerWithAddress[];
   let rewards: Contract;
   let token: Contract;
   let owner: SignerWithAddress;
@@ -152,7 +152,11 @@ describe("Token", function () {
 
     await token.connect(user2).transfer(user1.address, 8); // transfer all to existing
     expect((await token.getHolders()).length).to.equal(2);
-  });
+
+    holders = await token.getHolders();
+    // no tokens lost on the way
+    expect((await token.balanceOf(holders[0])).add(await token.balanceOf(holders[1]))).to.equal(tokenSupply);
+  });*/
 });
 
 describe("Rewards", function () {
@@ -167,7 +171,8 @@ describe("Rewards", function () {
   let initialBalanceUser1: BigNumber;
   let initialBalanceUser2: BigNumber;
   let initialBalanceUser3: BigNumber;
-  const tokenSupply = 1000;
+  const zero = ethers.BigNumber.from("0") as BigNumber;
+  const tokenSupply = 600;
 
   beforeEach(async function () {
     accounts = await ethers.getSigners();
@@ -177,11 +182,11 @@ describe("Rewards", function () {
     user3 = accounts[3];
 
     const tokenFact = await ethers.getContractFactory("MyToken");
-    token = await tokenFact.deploy(tokenSupply, "", "");
+    token = await tokenFact.deploy(tokenSupply, "", "", { gasPrice: 0 });
     await token.deployed();
 
     const rewardsFact = await ethers.getContractFactory("Rewards");
-    rewards = await rewardsFact.deploy(token.address);
+    rewards = await rewardsFact.deploy(token.address, { gasPrice: 0 });
     await rewards.deployed();
 
     initialBalanceOwner = await ethers.provider.getBalance(owner.address);
@@ -190,15 +195,42 @@ describe("Rewards", function () {
     initialBalanceUser3 = await ethers.provider.getBalance(user3.address);
   });
 
-  it("Initial check", async function () {
-    /*    const reward = 10000;
+  it("Single holder, gets all rewards", async function () {
+    const reward = 10000;
+    await rewards.notifyRewards({ value: reward, gasPrice: 0 });
+
+    const ownerBal = await ethers.provider.getBalance(owner.address);
+    expect(ownerBal).to.equal(initialBalanceOwner);
+  });
+
+  it("Two equal holders divide the reward equally", async function () {
+    const reward = 10000;
     await token.transfer(user1.address, tokenSupply / 2, { gasPrice: 0 });
     await rewards.notifyRewards({ value: reward, gasPrice: 0 });
 
     const ownerBal = await ethers.provider.getBalance(owner.address);
-    const user1Bal = await ethers.provider.getBalance(user1.address); */
-    //expect(ownerBal).to.equal(initialBalanceOwner.sub(reward).add(reward / 2));
-    //token.transfer(token.address, 10);
+    const user1Bal = await ethers.provider.getBalance(user1.address);
+
+    expect(ownerBal).to.equal(initialBalanceOwner.sub(reward).add(reward / 2));
+    expect(user1Bal).to.equal(initialBalanceUser1.add(reward / 2));
+  });
+
+  it("Three equal holders leave dust", async function () {
+    const reward = 10000;
+    await token.transfer(user1.address, tokenSupply / 3, { gasPrice: 0 });
+    await token.transfer(user2.address, tokenSupply / 3, { gasPrice: 0 });
+    await rewards.notifyRewards({ value: reward, gasPrice: 0 });
+
+    const ownerBal = await ethers.provider.getBalance(owner.address);
+    const user1Bal = await ethers.provider.getBalance(user1.address);
+    const user2Bal = await ethers.provider.getBalance(user2.address);
+    const contrBal = await ethers.provider.getBalance(rewards.address);
+
+    expect(ownerBal).to.equal(initialBalanceOwner.sub(reward).add(BigNumber.from((reward / 3).toFixed(0))));
+    expect(user1Bal).to.equal(initialBalanceUser1.add((reward / 3).toFixed(0)));
+    expect(user2Bal).to.equal(initialBalanceUser2.add((reward / 3).toFixed(0)));
+
+    expect(contrBal).to.gt(zero);
   });
   /* 
   it("No access", async function () {
