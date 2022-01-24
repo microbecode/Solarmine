717320dc83e222b35a3fd814cabebd9a001b5e45
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-01-24 17:01:14 +0200

    add reward test

--- contracts/SimpleRewards.sol
+++ contracts/SimpleRewards.sol
@@ -9,6 +9,14 @@ contract SimpleRewards {
         payable
         virtual
     {
+        // Make sure there is enough balance
+        uint256 total = 0;
+        for (uint256 i = 0; i < values.length; i++) {
+            total += values[i];
+        }
+        require(address(this).balance >= total, "Not enough balance");
+
+        // Send assets
         for (uint256 i = 0; i < receivers.length; i++) {
             (bool success, ) = receivers[i].call{value: values[i], gas: 3000}(
                 ""
@@ -18,6 +26,7 @@ contract SimpleRewards {
             }
         }
 
+        // Return remaining value to sender
         uint256 remaining = address(this).balance;
         if (remaining > 0) {
             (bool success, ) = msg.sender.call{value: remaining, gas: 3000}("");
--- test/SimpleRewards.test.ts
+++ test/SimpleRewards.test.ts
@@ -6,8 +6,12 @@ import { expect } from "chai";
 describe("Reward calculations", function () {
   let accounts: SignerWithAddress[];
   let rewards: Contract;
+  let reverter: Contract;
+
   let owner: SignerWithAddress;
   let user1: SignerWithAddress;
+
+  let currentCreatedAddressNumber: number = 0; // so that each test creates their own addresses
 
   beforeEach(async function () {
     accounts = await ethers.getSigners();
@@ -17,23 +21,84 @@ describe("Reward calculations", function () {
     const rewardsFact = await ethers.getContractFactory("SimpleRewards");
     rewards = await rewardsFact.deploy();
     await rewards.deployed();
+
+    const reverterFact = await ethers.getContractFactory("ReverterMock");
+    reverter = await reverterFact.deploy();
+    await reverter.deployed();
   });
 
-  it("Estimate gas costs", async function () {
+  /* it("Estimate gas costs", async function () {
     const amount = 500;
     const addrs = getAddresses(amount);
     const amounts = [...Array(amount).keys()].map((i) => i + 1);
 
-    await rewards.distribute(addrs, amounts);
-  });
-});
+    const totalAmounts = amounts.reduce((prev, curr) => (prev = prev + curr));
 
-const getAddresses = (amount: number): string[] => {
-  let prefixNum = ethers.BigNumber.from("10").pow(38);
-  const addrs: string[] = [];
-  for (let i = 0; i < amount; i++) {
-    const addr = "0x7" + prefixNum.add(i).toString();
-    addrs.push(addr);
-  }
-  return addrs;
-};
+    console.log("total", totalAmounts);
+
+    await rewards.distribute(addrs, amounts, { value: totalAmounts });
+  }); */
+
+  it("Single receiver, not enough balance", async function () {
+    const addrs = getAddresses(1);
+    const amounts = [2];
+
+    await expect(
+      rewards.distribute(addrs, amounts, { value: 1 })
+    ).to.be.revertedWith("Not enough balance");
+  });
+
+  it("Single receiver", async function () {
+    const addrs = getAddresses(1);
+    const amounts = [1];
+
+    await rewards.distribute(addrs, amounts, { value: 1 });
+
+    const balance = await ethers.provider.getBalance(addrs[0]);
+
+    expect(balance).to.equal(1);
+  });
+
+  it("Multiple receivers", async function () {
+    const addrs = getAddresses(3);
+    const amounts = [1, 2, 3];
+
+    await rewards.distribute(addrs, amounts, { value: 6 });
+
+    const balance1 = await ethers.provider.getBalance(addrs[0]);
+    const balance2 = await ethers.provider.getBalance(addrs[1]);
+    const balance3 = await ethers.provider.getBalance(addrs[2]);
+
+    expect(balance1).to.equal(1);
+    expect(balance2).to.equal(2);
+    expect(balance3).to.equal(3);
+  });
+
+  it("Multiple receivers, middle reverts", async function () {
+    const addrs = getAddresses(3);
+    const amounts = [1, 2, 3];
+
+    addrs[1] = reverter.address;
+
+    await rewards.distribute(addrs, amounts, { value: 6 });
+
+    const balance1 = await ethers.provider.getBalance(addrs[0]);
+    const balance2 = await ethers.provider.getBalance(addrs[1]);
+    const balance3 = await ethers.provider.getBalance(addrs[2]);
+
+    expect(balance1).to.equal(1);
+    expect(balance2).to.equal(0);
+    expect(balance3).to.equal(3);
+  });
+
+  const getAddresses = (amount: number): string[] => {
+    let prefixNum = ethers.BigNumber.from("10").pow(38);
+    const addrs: string[] = [];
+    for (let i = 0; i < amount; i++) {
+      const addr =
+        "0x7" + prefixNum.add(currentCreatedAddressNumber++).toString();
+      addrs.push(addr);
+    }
+    return addrs;
+  };
+});
